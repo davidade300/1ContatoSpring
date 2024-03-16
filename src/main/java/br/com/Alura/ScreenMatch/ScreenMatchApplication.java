703e2d878d220915f523f7638f666ecd94cca023
davidade300 <iaderinho@outlook.com>
2024-03-16 18:49:34 -0300

    .

--- src/main/java/br/com/Alura/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/Alura/ScreenMatch/ScreenMatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.Alura.ScreenMatch.model.DadosEpisodio;
 import br.com.Alura.ScreenMatch.model.DadosSerie;
 import br.com.Alura.ScreenMatch.service.ConsumeApi;
 import br.com.Alura.ScreenMatch.service.ConvertData;
@@ -26,6 +27,9 @@ public class ScreenMatchApplication implements CommandLineRunner {
 		ConvertData converter = new ConvertData();
 		DadosSerie dados = converter.getData(json, DadosSerie.class);
 		System.out.println(dados);
+		json = consumeApi.getData("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+		DadosEpisodio dadosEpisodio = converter.getData(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 
 }
